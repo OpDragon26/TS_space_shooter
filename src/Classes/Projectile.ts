56624bc557010cb4e28f91a226c8ebdd62cd98f2
szalai.mate <szalai.mate@students.jedlik.eu>
2026-02-10 13:26:01 +0100

    incomplete projectile

---
+++ src/Classes/Projectile.ts
@@ -0,0 +1,9 @@
+﻿import type SpaceShooter from "./SpaceShooter.ts";
+import Rectangle from "./Engine/Entities/Rectangle.ts";
+
+export default class Projectile extends Rectangle<SpaceShooter>
+{
+    constructor(x: number, y: number, game: SpaceShooter, tags: Set<number> = new Set()) {
+        super(x, y, 5, 5, 1, 0, game, "#FFFFFF", tags);
+    }
+}
